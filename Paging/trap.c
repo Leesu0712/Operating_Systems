8717aa0d264efc59dd8419f9ad52daa8a5f69cb6
Leesu0712 <82353043+Leesu0712@users.noreply.github.com>
2021-04-12 12:26:50 +0900

    Add files via upload

---
+++ Paging/trap.c
@@ -0,0 +1,146 @@
+#include "types.h"
+#include "defs.h"
+#include "param.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "proc.h"
+#include "x86.h"
+#include "traps.h"
+#include "spinlock.h"
+
+// Interrupt descriptor table (shared by all CPUs).
+struct gatedesc idt[256];
+extern uint vectors[];  // in vectors.S: array of 256 entry pointers
+struct spinlock tickslock;
+uint ticks;
+
+void
+tvinit(void)
+{
+  int i;
+
+  for(i = 0; i < 256; i++)
+    SETGATE(idt[i], 0, SEG_KCODE<<3, vectors[i], 0);
+  SETGATE(idt[T_SYSCALL], 1, SEG_KCODE<<3, vectors[T_SYSCALL], DPL_USER);
+
+  initlock(&tickslock, "time");
+}
+
+void
+idtinit(void)
+{
+  lidt(idt, sizeof(idt));
+}
+
+//PAGEBREAK: 41
+void
+trap(struct trapframe *tf)
+{
+  if(tf->trapno == T_SYSCALL){
+    if(proc->killed)
+      exit();
+    proc->tf = tf;
+    syscall();
+    if(proc->killed)
+      exit();
+    return;
+  }
+
+  // my own code for lazy paging
+  if(tf->trapno == T_PGFLT) {
+    // CR2 register contains the faulty memory address
+    uint cr2 = rcr2();
+
+    // Allocate a page for the faulty memory address below 
+    char *mem, *a;
+    pde_t *pde;
+    pte_t *pgtab, *pte;
+
+    // Allocate a page table for the faulty memory address (refer to allocuvm())
+    mem = kalloc();
+    memset(mem, 0, PGSIZE);
+
+    // Read the address of the PTE in page table pgdir (refer to walkpgdir())
+    a = (char*)PGROUNDDOWN(cr2);
+
+    pde = &(proc->pgdir)[PDX(a)];
+    if(*pde & PTE_P)
+       pgtab = (pte_t*)P2V(PTE_ADDR(*pde));
+    else {
+       pgtab = (pte_t*)kalloc();
+       memset(pgtab, 0, PGSIZE);
+       *pde = V2P(pgtab) | PTE_P | PTE_U | PTE_P;
+    }
+
+    pte = &pgtab[PTX(a)];
+
+    // Create PTEs for virtual addresses (refer to mappages()) 
+    *pte = V2P(mem) | PTE_W | PTE_U | PTE_P;
+    proc->newpgnum++;
+
+    return;
+  } 
+
+  switch(tf->trapno){
+  case T_IRQ0 + IRQ_TIMER:
+    if(cpunum() == 0){
+      acquire(&tickslock);
+      ticks++;
+      wakeup(&ticks);
+      release(&tickslock);
+    }
+    lapiceoi();
+    break;
+  case T_IRQ0 + IRQ_IDE:
+    ideintr();
+    lapiceoi();
+    break;
+  case T_IRQ0 + IRQ_IDE+1:
+    // Bochs generates spurious IDE1 interrupts.
+    break;
+  case T_IRQ0 + IRQ_KBD:
+    kbdintr();
+    lapiceoi();
+    break;
+  case T_IRQ0 + IRQ_COM1:
+    uartintr();
+    lapiceoi();
+    break;
+  case T_IRQ0 + 7:
+  case T_IRQ0 + IRQ_SPURIOUS:
+    cprintf("cpu%d: spurious interrupt at %x:%x\n",
+            cpunum(), tf->cs, tf->eip);
+    lapiceoi();
+    break;
+
+  //PAGEBREAK: 13
+  default:
+    if(proc == 0 || (tf->cs&3) == 0){
+      // In kernel, it must be our mistake.
+      cprintf("unexpected trap %d from cpu %d eip %x (cr2=0x%x)\n",
+              tf->trapno, cpunum(), tf->eip, rcr2());
+      panic("trap");
+    }
+    // In user space, assume process misbehaved.
+    cprintf("pid %d %s: trap %d err %d on cpu %d "
+            "eip 0x%x addr 0x%x--kill proc\n",
+            proc->pid, proc->name, tf->trapno, tf->err, cpunum(), tf->eip,
+            rcr2());
+    proc->killed = 1;
+  }
+
+  // Force process exit if it has been killed and is in user space.
+  // (If it is still executing in the kernel, let it keep running
+  // until it gets to the regular system call return.)
+  if(proc && proc->killed && (tf->cs&3) == DPL_USER)
+    exit();
+
+  // Force process to give up CPU on clock tick.
+  // If interrupts were on while locks held, would need to check nlock.
+  if(proc && proc->state == RUNNING && tf->trapno == T_IRQ0+IRQ_TIMER)
+    yield();
+
+  // Check if the process has been killed since we yielded
+  if(proc && proc->killed && (tf->cs&3) == DPL_USER)
+    exit();
+}
